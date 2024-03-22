d656ceff6fc18511448bcf47dc8f9d5cd564bdc8
Telekurysh <mic.mamaev@yandex.ru>
2024-03-22 13:12:04 +0300

    add admin-panel

--- test_project/admin.py
+++ test_project/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Product, Order, Review, UserProfile
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_at', 'updated_at')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'total_price', 'status', 'created_at', 'updated_at')
+    list_filter = ('status',)
+    search_fields = ('user__username',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'rating', 'created_at', 'updated_at')
+    list_filter = ('rating',)
+    search_fields = ('user__username', 'product__name')
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'shipping_address', 'phone_number')
+    search_fields = ('user__username', 'shipping_address')
